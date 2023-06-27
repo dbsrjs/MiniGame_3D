3363f9b97334101f8d68bfad2b2af18777ec8db9
xyl_t9 <dbsrjs1224@gmail.com>
2023-06-27 21:53:20 +0900

    06/27 MiniGame

--- Assets/0.Script/Game2/Bridge.cs
+++ Assets/0.Script/Game2/Bridge.cs
@@ -6,24 +6,34 @@ using UnityEngine.UI;
 public class Bridge : MonoBehaviour
 {
     [SerializeField] private GameObject[] brige = new GameObject[] {};
-
+    
     // Start is called before the first frame update
     void Start()
     {
+        int rand = Random.Range(0, 100);
+        bool a = rand < 50 ? true : false;
+
+        if (a == false)
+        {
+            
+        }
+        /*
         int halfCount = brige.Length / 2;
         for (int i = 0; i < halfCount; i++)
         {
             BoxCollider boxCollider = brige[i].GetComponent<BoxCollider>();
             if (boxCollider != null)
             {
-                boxCollider.isTrigger = true;
+                Destroy(boxCollider);
             }
         }
+        */
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
